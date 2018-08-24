cfabb0e8bc6ac8e766b293db48cabe9042272a95
Anix00011 <31801904+anix00011@users.noreply.github.com>
2018-08-24 13:15:29 +0530

    Create String.cpp

---
+++ String.cpp
@@ -0,0 +1,125 @@
+#std::string::compare
+
+Compares the value of the string object (or a substring) to the sequence of characters specified by its arguments.
+
+#include <iostream>
+#include <string>
+
+int main ()
+{
+  std::string str1 ("green apple");
+  std::string str2 ("red apple");
+
+  if (str1.compare(str2) != 0)
+    std::cout << str1 << " is not " << str2 << '\n';
+
+  if (str1.compare(6,5,"apple") == 0)
+    std::cout << "still, " << str1 << " is an apple\n";
+
+  if (str2.compare(str2.size()-5,5,"apple") == 0)
+    std::cout << "and " << str2 << " is also an apple\n";
+
+  if (str1.compare(6,5,str2,4,5) == 0)
+    std::cout << "therefore, both are apples\n";
+
+  return 0;
+}
+Edit & Run
+
+
+Output:
+green apple is not red apple
+still, green apple is an apple
+and red apple is also an apple
+therefore, both are apples
+//****************************************************************************************************************************************
+#size_t
+
+Unsigned integral type
+Alias of one of the fundamental unsigned integer types.
+It is a type able to represent the size of any object in bytes: size_t is the type returned by the sizeof operator and is widely used in the standard library to represent sizes and counts.
+//****************************************************************************************************************************************
+#std::string::find
+
+Searches the string for the first occurrence of the sequence specified by its arguments.
+
+#include <iostream>       // std::cout
+#include <string>         // std::string
+
+int main ()
+{
+  std::string str ("There are two needles in this haystack with needles.");
+  std::string str2 ("needle");
+
+  // different member versions of find in the same order as above:
+  std::size_t found = str.find(str2);
+  if (found!=std::string::npos)
+    std::cout << "first 'needle' found at: " << found << '\n';
+
+  found=str.find("needles are small",found+1,6);
+  if (found!=std::string::npos)
+    std::cout << "second 'needle' found at: " << found << '\n';
+
+  found=str.find("haystack");
+  if (found!=std::string::npos)
+    std::cout << "'haystack' also found at: " << found << '\n';
+
+  found=str.find('.');
+  if (found!=std::string::npos)
+    std::cout << "Period found at: " << found << '\n';
+
+  // let's replace the first needle:
+  str.replace(str.find(str2),str2.length(),"preposition");
+  std::cout << str << '\n';
+
+  return 0;
+}
+Edit & Run
+
+Output:
+first 'needle' found at: 14
+second 'needle' found at: 44
+'haystack' also found at: 30
+Period found at: 51
+There are two prepositions in this haystack with needles.
+//****************************************************************************************************************************************
+#std::string::substr
+
+Returns a newly constructed string object with its value initialized to a copy of a substring of this object.
+
+#include <iostream>
+#include <string>
+
+int main ()
+{
+  std::string str="We think in generalities, but we live in details.";
+                                           // (quoting Alfred N. Whitehead)
+
+  std::string str2 = str.substr (3,5);     // "think"
+
+  std::size_t pos = str.find("live");      // position of "live" in str
+
+  std::string str3 = str.substr (pos);     // get from "live" to the end
+
+  std::cout << str2 << ' ' << str3 << '\n';
+
+  return 0;
+}
+Edit & Run
+
+
+Output:
+think live in details.
+//****************************************************************************************************************************************
+
+//****************************************************************************************************************************************
+
+//****************************************************************************************************************************************
+
+//****************************************************************************************************************************************
+
+//****************************************************************************************************************************************
+
+//****************************************************************************************************************************************
+
+//****************************************************************************************************************************************
